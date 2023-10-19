5ffd7f8aab0d9588a081aa1d1572aff947c326e9
gabrielvbauer <gabriel.bauer9@hotmail.com>
2023-10-19 17:14:04 -0300

    feat: social login

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,62 @@
+import * as WebBrowser from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
+import { useEffect, useState } from 'react'
+import { Alert } from 'react-native'
+
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID } from '@env'
+
 import { Container, Title, Slogan } from './styles'
 
 import backgroundImg from '../../assets/background.png'
 import { Button } from '../../components/Button'
 
+WebBrowser.maybeCompleteAuthSession()
+
 export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false)
+
+  const [_, response, googleSignIn] = Google.useAuthRequest({
+    androidClientId: ANDROID_CLIENT_ID,
+    iosClientId: IOS_CLIENT_ID,
+    scopes: ['profile', 'email'],
+  })
+
+  function handleGoogleSignIn() {
+    setIsAuthenticating(true)
+
+    googleSignIn().then((response) => {
+      if (response.type !== 'success') {
+        setIsAuthenticating(false)
+      }
+    })
+  }
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      if (response.authentication?.idToken) {
+        console.log('TOKEN DE AUTENTICAÇÃO', response.authentication.idToken)
+        fetch(
+          `https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=${response.authentication.idToken}`,
+        )
+          .then((response) => response.json())
+          .then(console.log)
+      } else {
+        Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta Google')
+        setIsAuthenticating(false)
+      }
+    }
+  }, [response])
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
 
-      <Button title="Entrar com Google" />
+      <Button
+        title="Entrar com Google"
+        onPress={handleGoogleSignIn}
+        isLoading={isAuthenticating}
+      />
     </Container>
   )
 }
